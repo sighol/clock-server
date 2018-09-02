6e5f154f65d1517cd5798117335ddd7ee0c6021f
Sigurd Andreas Holsen <Sigurd.Andreas.Holsen@km.kongsberg.com>
2018-09-02 11:52:25 +0200

    first working version

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use hyper::rt::{self, Future};
 use hyper::service::service_fn_ok;
 use hyper::{Body, Response, Server};
 use std::net::SocketAddr;
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
+use std::thread;
 
 extern crate clap;
 use clap::{App, Arg, SubCommand};
@@ -23,28 +24,49 @@ fn main() {
         .subcommand(
             SubCommand::with_name("client")
                 .about("start client")
-                .arg(Arg::with_name("ip")),
+                .arg(Arg::with_name("ip").required(true).takes_value(true)),
         ).get_matches();
-
-    println!("Matches: {:?}", matches);
 
     if let Some(_server) = matches.subcommand_matches("server") {
         let addr = ([127, 0, 0, 1], 3001).into();
         start_server(addr);
     }
 
-    if let Some(_client) = matches.subcommand_matches("client") {
-        let r = reqwest::get("https://www.rust-lang.org");
-        println!("r: {:?}", r);
-        // let body = r?.text()?;
-        // println!("Received response: {}", body);
+    if let Some(client) = matches.subcommand_matches("client") {
+        let path = client.value_of("ip").unwrap();
+        let start = SystemTime::now();
+        let server_time = request_server_time(path)
+            .duration_since(SystemTime::UNIX_EPOCH).unwrap()
+            .as_millis() as i128;
+        let end = SystemTime::now();
+        let requst_duration = end.duration_since(start).unwrap().as_millis() as i128;
+
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH).unwrap()
+            .as_millis() as i128;
+
+        let real_server_time = server_time + requst_duration/2;
+        let time_diff = now - real_server_time;
+        println!("Time diff: {}ms", time_diff);
     }
+}
+
+fn request_server_time(path: &str) -> SystemTime {
+    let mut body = reqwest::get(path).expect("No result");
+    let text = body.text().expect("No text");
+    let millis =
+        u64::from_str_radix(&text, 10).expect("Bad response from server. Not clock sync server");
+    println!("Text: {}", millis);
+    let duration_since_epoch = Duration::from_millis(millis);
+    SystemTime::UNIX_EPOCH + duration_since_epoch
 }
 
 fn start_server(addr: SocketAddr) {
     let new_service = || {
         service_fn_ok(|_| {
+            thread::sleep(Duration::from_millis(150));
             let body = format!("{}", get_current_time());
+            thread::sleep(Duration::from_millis(150));
             Response::new(Body::from(body))
         })
     };
