65c6063ec28fa24a6cd1a82718e202cce30c4137
Sigurd Andreas Holsen <Sigurd.Andreas.Holsen@km.kongsberg.com>
2018-09-03 15:38:02 +0200

    remove dead code

--- src/bin/tcpclient/main.rs
+++ src/bin/tcpclient/main.rs
@@ -36,7 +36,6 @@ fn main() {
                 Ok(n) => {
                     if true {
                         let end = Utc::now();
-
                         let read_slice = &data[0..n];
                         let mut vec = vec![];
                         vec.extend_from_slice(&read_slice);
@@ -87,28 +86,3 @@ fn format_duration(dur: chrono::Duration) -> String {
     let sign = if is_neg { "-" } else { "" };
     format!("{}{}ms", sign, result)
 }
-
-// pub fn compute_diff(send_time: SystemTime, receive_time: SystemTime, server_time: SystemTime) {
-//     let request_duration_ms = receive_time.duration_since(send_time).unwrap().as_millis() as i128;
-//     let receive_time_ms = receive_time
-//         .duration_since(SystemTime::UNIX_EPOCH)
-//         .unwrap()
-//         .as_millis() as i128;
-
-//     println!("now: {}ms ", humantime::format_rfc3339_nanos(receive_time));
-//     println!(
-//         "server_time: {}ms",
-//         humantime::format_rfc3339_nanos(server_time)
-//     );
-
-//     let server_time_ms = server_time
-//         .duration_since(SystemTime::UNIX_EPOCH)
-//         .unwrap()
-//         .as_millis() as i128;
-
-//     println!("request_duration_ms: {}ms", request_duration_ms);
-
-//     let real_server_time = server_time_ms + request_duration_ms / 2;
-//     let time_diff = receive_time_ms - real_server_time;
-//     println!("\nTime diff: {}±{}ms", time_diff, request_duration_ms);
-// }
